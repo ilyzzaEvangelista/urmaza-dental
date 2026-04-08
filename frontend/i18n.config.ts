07667da6c1b18aeba6e28f3527f61654139009da
Ilyzza Evangelista <131341499+ilyzzaEvangelista@users.noreply.github.com>
2026-04-08 14:03:32 +0800

    Initial commit: Laravel backend & Nuxt 4 frontend with MySQL integration

---
+++ frontend/i18n.config.ts
@@ -0,0 +1,12 @@
+export default defineI18nConfig(() => ({
+  legacy: false,
+  locale: 'en',
+  messages: {
+    en: {
+      welcome: 'Welcome to Urmaza Dental Appointment System',
+    },
+    tl: {
+      welcome: 'Maligayang pagdating sa Urmaza Dental Appointment System',
+    }
+  }
+}))
---
+++ frontend/nuxt.config.ts
@@ -0,0 +1,47 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  ssr: false, // SPA style
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true },
+
+  modules: [
+    'vuetify-nuxt-module',
+    '@pinia/nuxt',
+    '@pinia-plugin-persistedstate/nuxt',
+    '@nuxtjs/i18n',
+    '@nuxtjs/color-mode',
+    '@nuxt/icon',
+    '@nuxt/image',
+    '@nuxt/eslint'
+  ],
+
+  css: [
+    '~/assets/scss/main.scss'
+  ],
+
+  i18n: {
+    locales: ['en', 'tl'],
+    defaultLocale: 'en',
+    vueI18n: './i18n.config.ts'
+  },
+
+  colorMode: {
+    preference: 'system',
+    fallback: 'light'
+  },
+
+  vuetify: {
+    moduleOptions: {
+      /* vuetify options */
+    }
+  },
+
+  vite: {
+    optimizeDeps: {
+      include: [
+        '@vue/devtools-core',
+        '@vue/devtools-kit'
+      ]
+    }
+  }
+})
